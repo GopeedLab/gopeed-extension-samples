fe14a13a648be3eaafe2b35f34b4a6fc0c9bdcfd
liwei <liwei-8466@qq.com>
2024-12-22 11:04:27 +0800

    feat: update gopeed-js version

--- github-contributor-avatars-sample/webpack.config.js
+++ github-contributor-avatars-sample/webpack.config.js
@@ -4,13 +4,13 @@ import GopeedPolyfillPlugin from 'gopeed-polyfill-webpack-plugin';
 
 const __dirname = fileURLToPath(import.meta.url);
 
-export default {
+export default (_, argv) => ({
   entry: './src/index.js',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, '../dist'),
   },
-  devtool: false,
+  devtool: argv.mode === 'production' ? undefined : false,
   plugins: [new GopeedPolyfillPlugin()],
   module: {
     rules: [
@@ -22,4 +22,4 @@ export default {
       },
     ],
   },
-};
+});
